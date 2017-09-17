cc1744e7d1d9ac1cc1397a58a6144df13af9ec66
Harvey Chan <ch4ch4@gmail.com>
2017-09-17 09:48:51 -0700

    :poop::poop::poop:

---
+++ app/src/main/java/com/example/hachan/flare_hashgraph/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hachan.flare_hashgraph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.swirlds.platform.
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
